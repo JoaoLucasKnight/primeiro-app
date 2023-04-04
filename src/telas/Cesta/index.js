733616914fd9bfa00b82e2bfa72546de2864ceac
JoaoLucasKnight <kirajota1@gmail.com>
2023-04-04 13:22:21 -0300

    deu certo

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,23 +1,43 @@
 
 import React from "react";
-import { View, StyleSheet, ScrollView } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
+
+import Te
 
 import Topo from './comp/topo';
 import Desc from './comp/desc';
-import Itens from './comp/item';
+import Item from './comp/item';
 
-export default function Cestas({topo, desc, itens}) {
-    return <ScrollView>
-         <Topo {...topo} />
+export default function Cesta({topo, desc, itens}) {
+    return <>
+        <FlatList
+        data={itens.lista}
+        renderItem={Item}
+        keyExtractor={({ nome }) => nome }
+        ListHeaderComponent={() => {
+            return <>
+            <Topo {...topo} />
             <View style={estilos.corpo}>
                 <Desc {...desc} />
-                <Itens {...itens} />
-            </View>  
-    </ScrollView>
+                <Texto style={estilos.titulo}>{ itens.titulo }</Texto>
+            </View>
+            </>
+        }}
+        />
+    </>
         
     };
     
     const estilos = StyleSheet.create({
+        titulo: {
+            color: "#464646",
+            fontWeight: "bold",
+            marginTop: 32,
+            marginBottom: 8,
+            fontSize: 20,
+            lineHeight: 32,
+          },
+
         corpo:{
             paddingVertical: 8,
             paddingHorizontal: 16,
